9f6481cb64c8836a98a7801cf861c33bff4a6758
ben <bentl22a9@gmail.com>
2024-07-24 15:55:29 +0900

    feat: add sendInitialFund script

---
+++ multi-sig-erc20-demo/scripts/sendInitialFundToSafe.ts
@@ -0,0 +1,37 @@
+import * as dotenv from "dotenv";
+import { ethers } from "hardhat";
+dotenv.config();
+
+async function main() {
+    
+    const RPC_URL = process.env.RPC_URL || "";
+    
+    const owner_1_pk = process.env.ACCOUNT_0_PK || "";
+    
+    const deployedSafeAddress: string = process.env.SAFE_PROXY_ADDRESS || "";
+
+    const ethersProvider = new ethers.JsonRpcProvider(RPC_URL);
+
+    const initialFundInWei = ethers.parseEther('1');
+
+    const txParam = {
+        to: deployedSafeAddress,
+        value: initialFundInWei
+    }
+
+    const owner1Signer = new ethers.Wallet(owner_1_pk, ethersProvider);
+
+    const tx = await owner1Signer.sendTransaction(txParam);
+
+    console.log(`Sending initial fund...`);
+    console.log(`Deposit tx: ${tx.hash}`);
+
+    const balanceInWei: bigint = await ethersProvider.getBalance(deployedSafeAddress);
+    const balance = ethers.formatEther(balanceInWei);
+    console.log(balance);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
